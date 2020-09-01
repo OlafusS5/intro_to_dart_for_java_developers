262e9be5ca5b077459310a074feadf7da5f4b141
Ondrej Karkoszka <karkoszkaondrej@gmail.com>
2020-09-01 18:01:55 +0200

    Standard dart project files

---
+++ bin/intro_to_dart_for_java_developers.dart
@@ -0,0 +1,5 @@
+import 'package:intro_to_dart_for_java_developers/intro_to_dart_for_java_developers.dart' as intro_to_dart_for_java_developers;
+
+void main(List<String> arguments) {
+  print('Hello world: ${intro_to_dart_for_java_developers.calculate()}!');
+}
---
+++ test/intro_to_dart_for_java_developers_test.dart
@@ -0,0 +1,8 @@
+import 'package:intro_to_dart_for_java_developers/intro_to_dart_for_java_developers.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
